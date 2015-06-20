2a41a53c2accb39334b9894ea24588bc39699478
kkrzys <klimekkrzys@gmail.com>
2015-06-20 18:25:15 +0200

    changed send email method (username from properties)

--- src/main/java/pl/edu/agh/toik/mail_notification/service/MailNotificationService.java
+++ src/main/java/pl/edu/agh/toik/mail_notification/service/MailNotificationService.java
@@ -1,6 +1,7 @@
 package main.java.pl.edu.agh.toik.mail_notification.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,13 @@ public class MailNotificationService {
     @Autowired
     private MailSender mailSender;
 
-    public void sendMailNotification(String to, String subject, String body) {
+    @Autowired
+    private Environment env;
+
+    public void sendMailNotification(String subject, String body) {
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setTo(to);
+        message.setTo(env.getRequiredProperty("mail.username"));
         message.setSubject(subject);
         message.setText(body);
 
